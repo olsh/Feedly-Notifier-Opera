c69f78f748c6218d60085d6b8bcc3e3a6dd3dd93
Oleg Shevchenko <shevchenko.oleg@outlook.com>
2014-08-03 16:43:57 +0400

    Added option to open all feeds when popup is opened.

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -19,6 +19,7 @@ var appGlobal = {
         maxNumberOfFeeds: 20,
         forceUpdateFeeds: false,
         useSecureConnection: true,
+        expandFeeds: false,
         isFiltersEnabled: false,
         filters: [],
         showCounter: true,
